adc87b746aed04d21aef893c852c88de8b37d71f
kemotlo022 <kemotlo022@student.wethinkcode.co.za>
2024-03-03 19:21:07 +0200

    able to add user to database and now i am going to start working on chat room UI

--- Sync-Chat-App/src/main/java/com/example/Sync/Chat/App/Routes/RouteController.java
+++ Sync-Chat-App/src/main/java/com/example/Sync/Chat/App/Routes/RouteController.java
@@ -1,9 +1,23 @@
 package com.example.Sync.Chat.App.Routes;
+import com.example.Sync.Chat.App.Database.UserDatabaseOperations;
+import com.example.Sync.Chat.App.Database.UserRepository;
+import com.example.Sync.Chat.App.Model.OnlineUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RouteController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public RouteController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/")
     public String splashPage(){
@@ -11,9 +25,24 @@ public class RouteController {
         return "splashPage";
     }
 
+    @GetMapping("/chatRoom")
+    public String chatRoom() {
+        return "chatRoom";
+    }
+
     @GetMapping("/registration")
-    public String registration(){
+    public String showRegistrationPage() {
         return "registration";
+    }
+
+
+
+    @PostMapping("/registration")
+    public String registration(@RequestParam String name, @RequestParam String username, @RequestParam String pwd) {
+//        CREATE A NEW USER and save the user
+        OnlineUser newUser = OnlineUser.createUser(name,username);
+        userRepository.save(newUser);
+        return "chatRoom";
     }
 
     @GetMapping("/login")
@@ -21,10 +50,10 @@ public class RouteController {
         return "login";
     }
 
-    @GetMapping("/error")
-    public String handleError() {
-        return "error";
-    }
+//    @GetMapping("/custom-error")
+//    public String handleError() {
+//        return "error";
+//    }
 
 
 }
--- Sync-Chat-App/src/main/java/com/example/Sync/Chat/App/SyncChatAppApplication.java
+++ Sync-Chat-App/src/main/java/com/example/Sync/Chat/App/SyncChatAppApplication.java
@@ -1,9 +1,13 @@
 package com.example.Sync.Chat.App;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+
 public class SyncChatAppApplication {
 
 	public static void main(String[] args) {
