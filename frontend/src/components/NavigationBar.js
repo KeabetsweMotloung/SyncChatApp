87b5f31de039a7f231bb0034edada858ad924b8a
kemotlo022 <kemotlo022@student.wethinkcode.co.za>
2024-03-10 17:30:26 +0200

    added navbar still needs more styling

---
+++ frontend/src/components/NavigationBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NavigationBar.css';  // Import the CSS file here
+import SplashLogo from './Splash.png';  // Assuming Splash.png is in the 'components' directory
+
+function NavigationBar() {
+  return (
+    <div className="header">
+      <img className="logo" src={SplashLogo} alt="" />
+      <div className="header__left">
+        <h2>SYNC CHAT</h2>
+      </div>
+    </div>
+  );
+}
+
+export default NavigationBar;
