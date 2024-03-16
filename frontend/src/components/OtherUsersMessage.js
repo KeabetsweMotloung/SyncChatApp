ac418fc88b6404bf9553644e3e5466e85c56d059
kemotlo022 <kemotlo022@student.wethinkcode.co.za>
2024-03-16 13:31:27 +0200

    Sample Ui is complete, now working on the backend to connect

--- frontend/src/components/OtherUsersMessage.js
+++ frontend/src/components/OtherUsersMessage.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './OtherUsersMessageStyle.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 function OtherUsersMessage() {
-    var sampleMessage = {name: "RandomUser", message: "This is my first message"};
+    var sampleMessage = {name: "Palesa Gumede", message: "This is my first message"};
     return(
-    <div className='OtherUsersMessage'>
-    <div className='conversationContainer'>
-    <p className='con-icon'>{sampleMessage.name[0]}</p>
-    <div className='other-text-content'>
-    <p className='con-title'>{sampleMessage.name}</p>
-    <p className='con-lastMessage'>{sampleMessage.message}</p>
-    <p className='con-timeStamp'>12:00am</p>
-            </div>
+        <div className='OtherUsersMessage'>
+
+                <div className='other-text-wrapper'>
+                    <div className='other-text-content'>
+                        <p className='con-title'>{sampleMessage.name}</p>
+                        <p className='con-lastMessage'>{sampleMessage.message}</p>
+                        <p className='con-timeStamp'>12:00am</p>
+                    </div>
+                </div>
+          
         </div>
-    </div>
     );
 }
 
